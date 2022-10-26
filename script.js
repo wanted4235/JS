f17967a3287b38eb79316b6b1b83e9ff7a45cf96
Wanted4235 <wanted_09@bk.ru>
2022-10-26 23:07:00 +0300

    Первое задание

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 'use strict'
 
-let con = confirm("How are you?");
+let money = prompt("Ваш бюджет на месяц в рублях?", "");
+let date = prompt("Введите дату в формате YYYY-MM-DD", "");
 
-let p = prompt("What do you do?", "nothing");
+let appData = {
+    budget: money,
+    timeData: date,
+    expenses: {}, //обязательные расходы
+    optionalExpenses: {}, //необязательные расходы
+    income: [],
+    savings: false,
+};
+
+let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    a2 = prompt('Во сколько обойдётся?', ''),
+    a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+    a4 = prompt('Во сколько обойдётся?', '');
+
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
