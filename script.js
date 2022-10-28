8713330ee53510f7757765e0818f73e0137b3445
Wanted4235 <wanted_09@bk.ru>
2022-10-28 15:28:53 +0300

    Практика по условиям и циклам

--- script.js
+++ script.js
@@ -12,12 +12,30 @@ let appData = {
     savings: false,
 };
 
-let a1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    a2 = prompt('Во сколько обойдётся?', ''),
-    a3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    a4 = prompt('Во сколько обойдётся?', '');
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+        b = +prompt('Во сколько обойдётся?', '');
 
-appData.expenses.a1 = a2;
-appData.expenses.a3 = a4;
+    if (typeof (a) === 'string' && typeof (a) != null && typeof (b) != null && a != '' && b != '' && a.length < 50) {
+        console.log('done');
+        appData.expenses[a] = b;
+    } else {
+        console.log('Неверный формат данных');
+        i--;
+    }
+};
 
-alert(appData.budget / 30);
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay <= 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData <= 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+}
+else {
+    console.log("Неверный тип данных");
+}
