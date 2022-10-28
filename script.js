50780a0fbcabee89a18e2d71b7f074a58e417c8a
Wanted4235 <wanted_09@bk.ru>
2022-10-28 20:18:31 +0300

    Домашнее задание по функциям

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let money, date;
 
+//функция инициализации бюджета и даты расчёта
 function start() {
     money = +prompt("Ваш бюджет на месяц в рублях?", "");
     date = prompt("Введите дату в формате YYYY-MM-DD", "");
@@ -11,9 +12,10 @@ function start() {
     }
 }
 
+//функция инициализации бюджета и даты расчёта
 start();
 
-
+//Объект счёта
 let appData = {
     budget: money,
     timeData: date,
@@ -23,8 +25,7 @@ let appData = {
     savings: true,
 };
 
-chooseExpenses();
-
+//Функция ввода обязательных расходов в месяц
 function chooseExpenses() {
     for (let i = 0; i < 2; i++) {
         let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
@@ -40,29 +41,60 @@ function chooseExpenses() {
     };
 }
 
-appData.moneyPerDay = (appData.budget / 30).toFixed();
+//Функция ввода обязательных расходов в месяц
+chooseExpenses();
 
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay <= 100) {
-    console.log("Минимальный уровень достатка");
-} else if (appData.moneyPerDay > 100 && appData <= 2000) {
-    console.log("Средний уровень достатка");
-} else if (appData.moneyPerDay > 2000) {
-    console.log("Высокий уровень достатка");
-}
-else {
-    console.log("Неверный тип данных");
+//Вывод ежедневного бюджета
+function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed();
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);
 }
 
+//Вывод ежедневного бюджета
+detectDayBudget();
+
+//Блок кода с расчетом уровня достатка
+function detectLevel() {
+    if (appData.moneyPerDay <= 100) {
+        console.log("Минимальный уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData <= 2000) {
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    }
+    else {
+        console.log("Неверный тип данных");
+    }
+}
+
+//Блок кода с расчетом уровня достатка
+detectLevel();
+
+//Расчёт накоплений в месяц с вклада
 function checkSavings() {
     if (appData.savings) {
         let save = +prompt("Ваша сумма накоплений?"),
             percent = +prompt("Под какой процент?");
 
         appData.monthIncome = save / 100 / 12 * percent;
-        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+        alert("Доход в месяц с вашего депозита: " + appData.monthIncome.toFixed(1));
     }
 }
 
+//Расчёт накоплений в месяц с вклада
 checkSavings();
+
+//Функция вычисления необяхательных расходов
+function chooseOptExpenses() {
+    for (let i = 1; i < 4; i++) {
+        let optExpensesItem = prompt("Статья необязательных расходов?");
+        if (typeof (optExpensesItem) == "string" && optExpensesItem != "" && optExpensesItem != null) {
+            appData.optionalExpenses[i] = optExpensesItem;
+            console.log("doneOptExpenses");
+        } else {
+            i--;
+        }
+    }
+}
+
+chooseOptExpenses();
